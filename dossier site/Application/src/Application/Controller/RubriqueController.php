4b7c3ff35a1693dd65efb5ded302ebd1ba1420b9
mik42000 <mikouille@gmail.com>
2015-11-16 10:12:43 +0100

    RubriqueController + layout + vue rubriqueselect

--- dossier site/Application/src/Application/Controller/RubriqueController.php
+++ dossier site/Application/src/Application/Controller/RubriqueController.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
 
 namespace Application\Controller;
 
@@ -21,7 +14,30 @@ class RubriqueController extends AbstractActionController
     public function rubriqueselectAction() 
     {        
         $rubriqueModel = new RubriqueModel();
-        $data = $rubriqueModel->fetchAll();
+        $data = null;
+        $rubrique = null;
+        try {
+            $data = $rubriqueModel->fetchAll();
+        }
+        catch(\Exception $e) {
+            $this->layout()->setVariable('listeRubrique', $data);
+            $this->layout()->setVariable('menu_id', 0);
+            //$this->layout()->setVariable('rubrique', null);
+            $this->layout()->setVariable('exception', $e->getMessage());
+            $this->layout()->setVariable('langue', $this->getEvent()->getRouteMatch()->getParam('langue'));
+            return new ViewModel(array('rubrique'=>null,'langue'=>$this->getEvent()->getRouteMatch()->getParam('langue')));
+        }
+        
+        try {
+            $rubrique = $rubriqueModel->findOne($this->getEvent()->getRouteMatch()->getParam('menu_id'));
+        } catch (\Exception $e) {
+            $this->layout()->setVariable('listeRubrique',$data);
+            $this->layout()->setVariable('menu_id', 0);
+            $this->layout()->setVariable('rubrique', $rubrique);
+            $this->layout()->setVariable('langue',$this->getEvent()->getRouteMatch()->getParam('langue'));
+            return new ViewModel(array('rubrique'=>null,'langue'=>$this->getEvent()->getRouteMatch()->getParam('langue'), 'exception' => $e->getMessage()));
+        }
+
 	$this->layout()->setVariable('listeRubrique',$data);
 	$rubrique = $rubriqueModel->findOne($this->getEvent()->getRouteMatch()->getParam('menu_id'));
 	$this->layout()->setVariable('menu_id',$rubrique['menu_id']);
