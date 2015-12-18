6c4da13fc5c0dcb5609d5d39cd543d2a4f6ff50a
mik42000 <mikouille@gmail.com>
2015-12-18 10:20:04 +0100

    public
    modifie le css

---
+++ public/js/ckeditor/config.js
@@ -0,0 +1,13 @@
+/**
+ * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+   config.filebrowserBrowseUrl = '../kcfinder-3.12/browse.php?opener=ckeditor&type=files';
+   config.filebrowserImageBrowseUrl = '../kcfinder-3.12/browse.php?opener=ckeditor&type=images';
+   config.filebrowserFlashBrowseUrl = '../kcfinder-3.12/browse.php?opener=ckeditor&type=flash';
+   config.filebrowserUploadUrl = '../kcfinder-3.12/upload.php?opener=ckeditor&type=files';
+   config.filebrowserImageUploadUrl = '../kcfinder-3.12/upload.php?opener=ckeditor&type=images';
+   config.filebrowserFlashUploadUrl = '../kcfinder-3.12/upload.php?opener=ckeditor&type=flash';
+};
